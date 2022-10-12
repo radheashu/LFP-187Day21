a48d0fd55e2a5d4a3d9148f50b353c21be2fc1b8
radheashu <ashwinihanwate1020@gmail.com>
2022-10-12 15:19:21 +0530

    added:New Programs

---
+++ MoodAnalyser.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class MoodAnalyser {
+    private String msg;
+    public MoodAnalyser() {
+    }
+
+    public MoodAnalyser(String msg){
+        this.msg = msg;
+    }
+
+    public String analyzeMood() {
+        try {
+            if(msg.contains("Sad")){
+                return "Sad";
+            }
+            return "Happy";
+        }catch (NullPointerException e){
+            return "Happy";
+        }
+    }
+}
+
